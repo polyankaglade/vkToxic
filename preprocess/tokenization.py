535f88712b7ec0945a7463b207ea5ffe7d285bdc
Anna Polyanskaya <31651460+polyankaglade@users.noreply.github.com>
2020-11-05 22:37:12 +0300

    fix typo

--- preprocess/tokenization.py
+++ preprocess/tokenization.py
@@ -7,7 +7,7 @@ def tokenize(text_data):
     text_data = re.sub('\[id\d*\|\w*\]', 'username', text_data) # замена всех упоминаний пользователей
     text_data = re.sub('"', '', text_data) # удаление двойных кавычек
     text_data = re.sub("'", '', text_data) # удаление апострофов
-    text_data = re.sub('ё', 'е', text_data) # замена Е на Ё
+    text_data = re.sub('ё', 'е', text_data) # замена Ё на Е
     text_data = re.sub(r'(?<=\w)\*', 'ё', text_data) # замена * на Ё, если перед ним буква
     tokens = simple_word_tokenize(text_data.lower()) # токенизация
     return ' '.join(tokens)
