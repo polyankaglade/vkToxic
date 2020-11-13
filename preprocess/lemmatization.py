94c3a1c7985cfbaff8bdc8130e94f1c26e4e4b2c
Anna Scherbakova <31651581+Aniezka@users.noreply.github.com>
2020-11-13 15:00:28 +0300

    Update lemmatization.py
    +

--- preprocess/lemmatization.py
+++ preprocess/lemmatization.py
@@ -3,6 +3,6 @@ from pymorphy2 import MorphAnalyzer
 morph = MorphAnalyzer()
 
 def lemmatizer(text_data: str):
-text_data = text_data.split()
-lemms = [morph.parse(i.text)[0].normal_form for i in text_data]
-return " ".join(lemms)
+  text_data = text_data.split()
+  lemms = [morph.parse(i.text)[0].normal_form for i in text_data]
+  return " ".join(lemms)
