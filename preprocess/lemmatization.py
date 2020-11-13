55f5cb39a36e2b42a9e3bbea9b44080ec609d602
Anna Scherbakova <31651581+Aniezka@users.noreply.github.com>
2020-11-13 18:54:22 +0300

    Update lemmatization.py
    +

--- preprocess/lemmatization.py
+++ preprocess/lemmatization.py
@@ -6,3 +6,9 @@ def lemmatizer(text_data: str):
   text_data = text_data.split()
   lemms = [morph.parse(i.text)[0].normal_form for i in text_data]
   return " ".join(lemms)
+
+
+"""
+usage:
+lemms_string = lemmatizer(text_string)
+"""
