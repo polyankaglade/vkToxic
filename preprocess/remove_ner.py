f2290b2fb06723aad35059bf735eca9e7c4c95ec
eoershova <35444491+eoershova@users.noreply.github.com>
2020-11-24 02:45:18 +0300

    fix download

--- preprocess/remove_ner.py
+++ preprocess/remove_ner.py
@@ -1,5 +1,5 @@
 import re
-import urllib
+from urllib.request import urlretrieve
 import os.path
 
 from navec import Navec
@@ -8,14 +8,15 @@ from slovnet import NER
 destination = 'navec_news_v1_1B_250K_300d_100q.tar'
 
 if os.path.isfile(destination):
-	continue
+    pass
 else:
-	url = 'https://storage.yandexcloud.net/natasha-navec/packs/navec_news_v1_1B_250K_300d_100q.tar'
-	urllib.urlretrieve(url, destination)
+    url = 'https://storage.yandexcloud.net/natasha-navec/packs/navec_news_v1_1B_250K_300d_100q.tar'
+    urlretrieve(url, destination)
 
 navec = Navec.load('navec_news_v1_1B_250K_300d_100q.tar')
 ner = NER.load('slovnet_ner_news_v1.tar')
 ner.navec(navec)
+
 
 def remove_ner(text):
         ner_spans = ner(text).spans
